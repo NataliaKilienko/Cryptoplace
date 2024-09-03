b1fa387fe9e5e6508f01f936741a29004105506c
NataliaKilienko <kilienko900@gmail.com>
2024-09-03 22:45:59 +0300

    Update base path for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+     <BrowserRouter basename="/cryptoplace"> 
       <CoinContextProvider>
         <App />
       </CoinContextProvider>
